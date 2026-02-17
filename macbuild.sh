5ce54df674a77b6b7d832771e75ad17b3bd6d96e
重装小杰 <73771431+beiklive@users.noreply.github.com>
2026-02-17 12:20:45 +0800

    fix: 修复macbuild.sh脚本，添加打开New_mGBA.app的功能
    在macbuild.sh脚本中，添加了打开构建生成的New_mGBA.app应用程序的命令。该命令会在构建完成后自动打开应用程序，以便开发者可以立即测试构建结果。

--- macbuild.sh
+++ macbuild.sh
@@ -1,2 +1,3 @@
 cmake -B build_macos -DCMAKE_BUILD_TYPE=Release -DPLATFORM_DESKTOP=ON -DBUNDLE_MACOS_APP=ON -DUSE_GLFW=ON
 make -C build_macos -j$(sysctl -n hw.ncpu)
+open ./build_macos/New_mGBA.app

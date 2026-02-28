e37a3070d0b0de39e4067f2e133552352c46b021
beiklive <1376896787@qq.com>
2026-02-28 19:53:30 +0800

    feat: 添加开发容器配置，更新构建脚本，删除不必要的库文件

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cmake -B build_switch -DCMAKE_BUILD_TYPE=Release -DPLATFORM_SWITCH=ON -DUSE_DEKO3D=ON &&
+cmake -B build_switch -DCMAKE_BUILD_TYPE=Release -DPLATFORM_SWITCH=ON &&
 make -C build_switch New_mGBA.nro -j$(nproc)
